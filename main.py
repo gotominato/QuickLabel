bfa074a402a6b6bda8ddc4b9643b71629a9da154
zenifa317 <zenifa2001317@gmail.com>
2025-07-07 11:49:26 +0900

    ラベリングツールを実行するためのスクリプトを追加

---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+
+from tool import LabelingTool
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run the labeling tool.")
+    parser.add_argument('--folder', type=str, required=True, help='Path to the folder containing images.')
+    parser.add_argument('--mode', type=str, default=None, choices=['single', 'multi', 'add_label'], help='Mode of operation: single, multi, or add_label.')
+    args = parser.parse_args()
+    
+    folder = args.folder
+    mode = args.mode
+
+    tool = LabelingTool(folder=folder, mode=mode)
+    tool.run()
