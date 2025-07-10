58d7a5a588ad53a22b21f2c8968bf84f1c0a23c7
zenifa317 <zenifa2001317@gmail.com>
2025-07-10 14:20:19 +0900

    feat(labeling_tool): モード引数を必須に変更
    main.pyの引数解析部分で、モード引数を必須に設定しました。これにより、ユーザーがモードを指定しない場合にエラーが発生するようになります。

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tool import LabelingTool
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run the labeling tool.")
     parser.add_argument('--folder', type=str, required=True, help='Path to the folder containing images.')
-    parser.add_argument('--mode', type=str, default=None, choices=['single', 'multi', 'add_label'], help='Mode of operation: single, multi, or add_label.')
+    parser.add_argument('--mode', type=str, required=True, default=None, choices=['single', 'multi', 'add_label'], help='Mode of operation: single, multi, or add_label.')
     args = parser.parse_args()
     
     folder = args.folder
