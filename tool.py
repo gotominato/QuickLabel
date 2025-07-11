8281f3e87194d0f52e9bfc6c34a244a2bbe62c62
zenifa317 <zenifa2001317@gmail.com>
2025-07-11 21:09:53 +0900

    feat: クラス名の変更とラベル機能の修正
    - ラベル機能をクラス機能に変更し、関連するメソッドと変数名を更新しました。
    - モード選択の条件を修正し、クラスの追加・削除機能を実装しました。
    - 表示データにクラスリストを追加し、エラーメッセージを適切に更新しました。

--- tool.py
+++ tool.py
@@ -18,8 +18,8 @@ class LabelingTool:
         
         self.states = self.data_manager.load_states()
         self.annotations = self.data_manager.load_annotation()
-        self.label_list = self.data_manager.load_labels()
-        
+        self.classes_list = self.data_manager.load_classes()
+
         self.current_index = -1
         self.current_image_name = None
         
@@ -41,8 +41,8 @@ class LabelingTool:
             cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
             cv2.resizeWindow(self.window_name, 800, 600)
             self._label_image()
-        elif self.mode == 'add_label':
-            self._add_label()
+        elif self.mode == 'add_class':
+            self._add_class()
         else:
             self.message = f'エラー: モードが指定されていないか、不明なモード "{self.mode}" です。'
             self.terminal_view.show_message(self.message)
@@ -65,16 +65,19 @@ class LabelingTool:
                 'mode': self.mode,
                 'total_images': total_images,
                 'current_index': self.current_index,
-                'label_list': self.label_list['labels'],
+                'class_list': self.classes_list['classes'],
                 'labels': self.annotations['annotations'].get(self.current_image, []),
-                'image': self.current_image
+                'image': self.current_image,
+                'message': self.message
             }
+            
+            self.terminal_view.render(display_data, self.mode)
+            
+            self.message = ''
 
             if self.current_index != temp_index:
                 self._show_image(image_path)
             cv2.waitKey(1)
-
-            self.terminal_view.render(display_data, self.mode)
 
             command = self.terminal_view.get_input('> ')
             self._process_label_command(command)
@@ -84,81 +87,94 @@ class LabelingTool:
 
     def _process_label_command(self, command: str) -> None:
         command = command.replace(',', ' ')
-        actions = command.split()
-        
-        if actions[0].isdecimal():
+        parts = command.split()
+        action = parts[0].lower()
+
+        if action.isdecimal():
             if self.mode == 'single':
-                if self.annotations['annotations'].get(self.current_image, []) > 0:
+                if len(self.annotations['annotations'].get(self.current_image, [])) > 0:
                     self.message = f'エラー: 既にラベルが付けられています。'
                 else:
-                    if len(actions) != 1:
-                        self.message = f'エラー: ラベル番号を一つだけ入力してください。'
+                    if len(parts) != 1:
+                        self.message = f'エラー: クラス番号を一つだけ入力してください。'
                     else:
-                        if int(actions[0]) < 1 or int(actions[0]) > len(self.label_list['labels']):
-                            self.message = f'エラー: ラベル番号は1から{len(self.label_list["labels"])}の範囲で入力してください。'
+                        if int(action) < 1 or int(action) > len(self.classes_list['classes']):
+                            self.message = f'エラー: クラス番号は1から{len(self.classes_list["classes"])}の範囲で入力してください。'
                         else:
-                            self._update_annotation(actions[0], action='add')
-                            
-            elif self.mode == 'multi':
-                for label in actions:
-                    if int(label) < 1 or int(label) > len(self.label_list['labels']):
-                        self.message = f'エラー: ラベル番号は1から{len(self.label_list["labels"])}の範囲で入力してください。'
-                    else:
-                        self._update_annotation(label, action='add')
+                            self._update_annotation(action, action='add')
+                            self.message = f'クラス "{self.classes_list["classes"][int(action) - 1]}" を追加しました。'
 
-        elif actions[0].lower() == 'n':
+            elif self.mode == 'multi':
+                for class_id in parts:
+                    if int(class_id) < 1 or int(class_id) > len(self.classes_list['classes']):
+                        self.message = f'エラー: クラス番号は1から{len(self.classes_list["classes"])}の範囲で入力してください。'
+                    else:
+                        self._update_annotation(class_id, action='add')
+                self.message = f'クラス "{", ".join([self.classes_list["classes"][int(c) - 1] for c in parts])}" を追加しました。'
+
+        elif action == 'n':
             self._update_states(1)
 
-        elif actions[0].lower() == 'p':
+        elif action == 'p':
             self._update_states(-1)
 
-        elif actions[0].lower() == 's':
-            if len(actions) != 1:
+        elif action == 's':
+            if len(parts) != 1:
                 result = {}
-                for s_label in actions[1:]:
-                    result.update(self.data_manager.search_label(self.label_list, s_label))
-                    
+                for s_class in parts[1:]:
+                    result.update(self.data_manager.search_class(self.classes_list, s_class))
+
                 if not result:
                     self.message = f'エラー： 検索結果が見つかりません。'
                 else:
                     self.message = f'検索結果: {" ".join([f"{k}: {v}" for k, v in result.items()])}'      
             else:
-                self.message = f'エラー: 検索するラベル名が入力されていません。'
-        
-        elif actions[0].lower() == 'a':
-            if len(actions) != 1:
-                for label in actions[1:]:
-                    self._update_label_list(label, action='add')
-                self.message = f'ラベル "{", ".join(actions[1:])}" を追加しました。'
+                self.message = f'エラー: 検索するクラス名が入力されていません。'
+
+        elif action == 'a':
+            if len(parts) != 1:
+                for class_name in parts[1:]:
+                    self._update_class_list(class_name, action='add')
+                self.message = f'クラス "{", ".join(parts[1:])}" を追加しました。'
             else:
-                self.message = f'エラー: ラベル名が入力されていません。'
-        
-        elif actions[0].lower() == 'r':
-            if len(actions) != 1:
-                for label in actions[1:]:
-                    self._update_annotation(label, action='remove')
+                self.message = f'エラー: クラス名が入力されていません。'
+
+        elif action == 'r':
+            if len(parts) != 1:
+                del_labels = []
+                labels = list(map(int, parts[1:]))
+                labels.sort()
+                labels.reverse()
+                labels = list(map(str, labels))
+                for label in labels:
+                    if len(self.classes_list['classes']) < int(label)-1 or int(label) < 1:
+                        self.message = f'エラー: 削除するクラス番号は1から{len(self.classes_list["classes"])}の範囲で入力してください。'
+                    else:
+                        del_labels.append(self.classes_list['classes'][int(label) - 1])
+                        self._update_annotation(label, action='remove')
+                self.message = f'クラス "{", ".join(del_labels)}" を削除しました。'
             else:
                 self.message = f'エラー: 削除するラベル名が入力されていません。'
-        
-        elif actions[0].lower() == 'q':
+
+        elif action == 'q':
             self.quit_flag = True
-            self.terminal_view.show_message("保存して終了します。")
-        
+            self.terminal_view.show_message('保存して終了します。')
+
         else:
             self.message = f'エラー: コマンドが入力されていないか、不明なコマンド"{command}"です。'
             
         self.data_manager.save_states(self.states)
         self.data_manager.save_annotation(self.annotations)
-        self.data_manager.save_label_list(self.label_list)
+        self.data_manager.save_class_list(self.classes_list)
 
-    def _add_label(self) -> None:
+    def _add_class(self) -> None:
 
-        self.message = "追加したいラベル名を入力してください。"
+        self.message = '追加したいクラス名を入力してください。'
 
         while True:
             display_data = {
                 'mode': self.mode,
-                'label_list': self.label_list['labels'],
+                'class_list': self.classes_list['classes'],
                 'message': self.message
             }
             self.message = ''
@@ -166,42 +182,50 @@ class LabelingTool:
 
             command = self.terminal_view.get_input('> ')
 
-            self._process_add_label_command(command)
+            self._process_add_class_command(command)
 
             if self.quit_flag:
                 break
 
-    def _process_add_label_command(self, command: str) -> None:
+    def _process_add_class_command(self, command: str) -> None:
         command = command.replace(',', ' ')
-        parts = command.split(' ', 1)
+        parts = command.split(' ')
         action = parts[0].lower()
 
         if action == 'q':
-            self.terminal_view.show_message("ラベルリストを保存して終了します。")
+            self.terminal_view.show_message('クラスリストを保存して終了します。')
             self.quit_flag = True
 
         elif action == 'd':
             if len(parts) > 1:
-                labels = parts[1: ]
-                for label_to_delete in labels:
-                    self._update_label_list(label_to_delete, action='remove')
-                    self.message = f"'{label_to_delete}' を削除しました。"
+                del_classes = []
+                classes = list(map(int, parts[1:]))
+                classes.sort()
+                classes.reverse()
+                classes = list(map(str, classes))
+                for class_to_delete in classes:
+                    if len(self.classes_list['classes']) < int(class_to_delete)-1 or int(class_to_delete) < 1:
+                        self.message = f'エラー: 削除するクラス番号は1から{len(self.classes_list["classes"])}の範囲で入力してください。'
+                    else:
+                        del_classes.append(self.classes_list['classes'][int(class_to_delete) - 1])
+                        self._update_class_list(class_to_delete, action='remove')
+                self.message = f'クラス "{", ".join(del_classes)}" を削除しました。'
 
             else:
-                self.message = "エラー: 削除するラベル名を指定してください。"
+                self.message = 'エラー: 削除するクラス名を指定してください。'
 
         else:
             command = command.replace(',', ' ')
-            new_label = command.split()
-            if new_label in self.label_list['labels']:
-                self.message = f"エラー: '{new_label}' は既に存在します。"
+            new_class = command.split()
+            if new_class in self.classes_list['classes']:
+                self.message = f'エラー: "{new_class}" は既に存在します。'
             else:
-                for label in new_label:
-                    self._update_label_list(label, action='add')
-                new_label = ', '.join(new_label)
-                self.message = f"'{new_label}' を追加しました。"
-        
-        self.data_manager.save_label_list(self.label_list)
+                for class_to_add in new_class:
+                    self._update_class_list(class_to_add, action='add')
+                new_class = ', '.join(new_class)
+                self.message = f'クラス "{new_class}" を追加しました。'
+
+        self.data_manager.save_class_list(self.classes_list)
 
     def _update_states(self, number: int) -> None:
         self.states['last_processed_index'] +=  number
@@ -209,24 +233,24 @@ class LabelingTool:
     def _update_annotation(self, label: str, action: str) -> None:
         if self.current_image not in self.annotations['annotations']:
             self.annotations['annotations'][self.current_image] = []
-            
-        label_name = self.label_list['labels'][int(label) - 1]
+
+        class_name = self.classes_list['classes'][int(label) - 1]
         if action == "add":
-            if label_name not in self.annotations['annotations'][self.current_image]:
-                self.annotations['annotations'][self.current_image].append(label_name)
+            if class_name not in self.annotations['annotations'][self.current_image]:
+                self.annotations['annotations'][self.current_image].append(class_name)
 
         elif action == "remove":
-            if label_name in self.annotations['annotations'][self.current_image]:
-                self.annotations['annotations'][self.current_image].remove(label_name)
+            if class_name in self.annotations['annotations'][self.current_image]:
+                self.annotations['annotations'][self.current_image].remove(class_name)
 
-    def _update_label_list(self, label: str, action: str) -> None:
+    def _update_class_list(self, class_name: str, action: str) -> None:
         if action == "add":
-            if label not in self.label_list['labels']:
-                self.label_list['labels'].append(label)
+            if class_name not in self.classes_list['classes']:
+                self.classes_list['classes'].append(class_name)
         elif action == "remove":
-            num_label = int(label)
-            if 0 <= num_label - 1 < len(self.label_list['labels']):
-                del self.label_list['labels'][num_label - 1]
+            num_class = int(class_name)
+            if 0 <= num_class - 1 < len(self.classes_list['classes']):
+                del self.classes_list['classes'][num_class - 1]
 
     def _show_image(self, image_path: str) -> None:
         try:
@@ -235,4 +259,4 @@ class LabelingTool:
                 raise FileNotFoundError
             cv2.imshow(self.window_name, img)
         except Exception as e:
-            self.terminal_view.show_message(f"画像を表示できません: {e}")
+            self.terminal_view.show_message(f'画像を表示できません: {e}')
